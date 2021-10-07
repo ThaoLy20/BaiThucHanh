11a55819b9c72f5e91aa43b0614b421a7911d828
ThaoLy20 <92066719+ThaoLy20@users.noreply.github.com>
2021-10-07 13:34:20 +0700

    Add files via upload

---
+++ DuAnBUS.cs
@@ -0,0 +1,39 @@
+﻿using QuanLyNhanSu.DataAccess.DAL;
+using QuanLyNhanSu.DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu.Business
+{
+    class DuAnBUS
+    {
+        DuAnDAL dadal = new DuAnDAL();
+        Data dt = new Data();
+        public DataTable getDuAn()
+        {
+            return dadal.getDuAn();
+        }
+       public void insertDA(DuAnDTO da)
+        {
+             dadal.insertDA(da);
+        }
+        public void updateDA(DuAnDTO da, string ma)
+        {
+            dadal.updateDA(da, ma);
+        }
+        public void deleteDA(string ma)
+        {
+            dadal.deleteDA(ma);
+        }
+        public DataTable Search(String condi)
+        {
+            return dadal.Search(condi);
+        }
+    }
+}
---
+++ LuongBUS.cs
@@ -0,0 +1,54 @@
+﻿using QuanLyNhanSu.DataAccess.DAL;
+using QuanLyNhanSu.DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.Business
+{
+    class LuongBUS
+    {
+        LuongDAL luongdal = new LuongDAL();
+        
+        Data dt = new Data();
+        public DataTable getLuong()
+        {
+            return luongdal.getLuong();
+        }
+        public DataTable GetLuongtheoManv(string manv)
+        {
+            return luongdal.GetLuongtheoManv(manv);
+        }
+        public DataTable GetLuongtheoTG(int thang, int nam)
+        {
+            return luongdal.GetLuongtheoTG(thang, nam);
+        }
+        public void insertL(LuongDTO l)
+        {
+            luongdal.insertL(l);
+        }
+        public void updateL(LuongDTO l, string ma)
+        {
+            luongdal.updateL( l, ma);
+        }
+        public void deleteL(string ma)
+        {
+            luongdal.deleteL(ma);
+        }
+        public DataTable Search(String condi)
+        {
+            return luongdal.Search(condi);
+        }
+        public bool CheckMa(string maluong, string manhanvien)
+        {
+            return luongdal.CheckMa(manhanvien, maluong);
+        }
+        public bool CheckThangNam(string manv,int Thang,int Nam)
+        {
+            return luongdal.CheckThangNam(manv,Thang,Nam);
+        }
+    }
+}
